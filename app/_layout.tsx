70223f8e6a78377ce77d7875f2d0778eeb77a470
tran-thanh24 <148622471+tran-thanh24@users.noreply.github.com>
2026-03-18 23:16:15 +0700

    fix login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,17 @@ import { useEffect, useState } from "react";
 
 export default function RootLayout() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isReady, setIsReady] = useState(false);
   const segments = useSegments();
   const router = useRouter();
 
   useEffect(() => {
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isReady) return;
+
     const inAuthGroup = segments[0] === "auth";
 
     if (!isLoggedIn && !inAuthGroup) {
@@ -14,13 +21,12 @@ export default function RootLayout() {
     } else if (isLoggedIn && inAuthGroup) {
       router.replace("/(tabs)/index");
     }
-  }, [isLoggedIn, segments]);
+  }, [isLoggedIn, segments, isReady]);
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* Khai báo tất cả các route khả dụng */}
-      <Stack.Screen name="auth/login/index" />
-      <Stack.Screen name="auth/register/index" />
+      <Stack.Screen name="auth/login" />
+      <Stack.Screen name="auth/register" />
       <Stack.Screen name="(tabs)" />
       <Stack.Screen
         name="add-expense/index"
